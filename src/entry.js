3f2c4998f16a8ad599790dd477c6b46604f88bf0
vvacarov <vervac@mail.ru>
2020-08-17 12:51:26 +0300

    commented globals.vue

--- src/entry.js
+++ src/entry.js
@@ -15,15 +15,15 @@ const plugin = {
 
 // To auto-install when vue is found
 /* global window global */
-let GlobalVue = null;
-if (typeof window !== 'undefined') {
-  GlobalVue = window.Vue;
-} else if (typeof global !== 'undefined') {
-  GlobalVue = global.Vue;
-}
-if (GlobalVue) {
-  GlobalVue.use(plugin);
-}
+// let GlobalVue = null;
+// if (typeof window !== 'undefined') {
+//   GlobalVue = window.Vue;
+// } else if (typeof global !== 'undefined') {
+//   GlobalVue = global.Vue;
+// }
+// if (GlobalVue) {
+//   GlobalVue.use(plugin);
+// }
 
 // Inject install function into component - allows component
 // to be registered via Vue.use() as well as Vue.component()
